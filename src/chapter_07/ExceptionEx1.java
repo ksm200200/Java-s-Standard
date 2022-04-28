a2a725013ff59983574bac985c6aadf5a6bb3dd3
sunmin <ksm200200@naver.com>
2022-04-28 23:10:30 +0900

    2022.04.28

---
+++ src/chapter_07/ExceptionEx1.java
@@ -0,0 +1,15 @@
+package chapter_07;
+
+public class ExceptionEx1 {
+	
+	public static void main(String[] args) {
+		try {
+			try {} catch (Exception e) {} 
+			try {} catch (Exception e2) {}
+		} catch (Exception e) {
+		
+		}  //try - catch의 끝
+		
+	}// main 끝
+	
+}
---
+++ src/chapter_07/ExceptionEx2.java
@@ -0,0 +1,14 @@
+package chapter_07;
+
+public class ExceptionEx2 {
+	public static void main(String[] args) {
+		int number = 100;
+		int result = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			result = number / (int)(Math.random() * 10);  // 100 나누기 (0~9)
+			System.out.println(result); //0으로 나눌시 오류
+		}
+	}
+
+}
